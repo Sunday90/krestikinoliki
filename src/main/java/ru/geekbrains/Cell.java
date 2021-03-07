b055fa8b1f75015c95cc74cda2e28c0dd0848d22
Konstantin Plisov <3dnow@bk.ru>
2021-03-07 21:55:26 +0500

    dev commit

---
+++ src/main/java/ru/geekbrains/Cell.java
@@ -0,0 +1,11 @@
+package ru.geekbrains;
+
+public class Cell {
+    int column;
+    int string;
+
+    public Cell (int column, int string) {
+        this.column = column;
+        this.string = string;
+    }
+}
---
+++ src/main/java/ru/geekbrains/Combination.java
@@ -0,0 +1,13 @@
+package ru.geekbrains;
+
+public class Combination {
+    String start;
+    String end;
+    CombType combType;
+
+    public Combination (String start, String end, CombType combType) {
+        this.start = start;
+        this.end = end;
+        this.combType = combType;
+    }
+}
